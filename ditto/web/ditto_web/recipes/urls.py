e0cb6491155c67b6d502b39200d296b18da771c2
zeddy <wweg@email.cz>
2026-03-25 18:51:28 +0100

    cleanup: removed unnecessary files and added gitignore

--- ditto/web/ditto_web/recipes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = "recipes"
-
-urlpatterns = [
-    path("", views.recipe_list, name="list"),
-    path("<int:pk>/", views.recipe_detail, name="detail"),
-]
